f6788282d516d1a680ecc2f85a4717a2e0f5ac60
Ullov <dorogobed.sasha@gmail.com>
2022-01-23 22:06:41 +0300

    All files in place but it still does not work

---
+++ BookCollection/cpp_h/main.cpp
@@ -0,0 +1,54 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QUuid>
+#include <QQmlEngine>
+/*#include "KTools/uuidslist.h"
+#include "KTools/options.h"
+#include "KTools/Kff/metainfofs.h"
+#include "KTools/Kff/manager.h"
+#include "KTools/Kff/rawstream.h"
+#include "KTools/Kff/fixedtypes.h"
+#include "KTools/Kff/variabletypes.h"
+*/
+#include <libKTools>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    KTools::Kff::registerTypesForQml();
+
+    static KTools::Options *options = new KTools::Options();
+    FsHandler *fsExplorerHandle = new FsHandler();
+    KTools::Kff::MetainfoFs db(options);
+    ImageStorage::Interface imageStorage(&db);
+
+    UuidsList::ids.init(&UuidsList::ids);
+    UuidsList::ids.createItems(20);
+
+    engine.rootContext()->setContextProperty("uuidsList", &UuidsList::ids);
+    engine.rootContext()->setContextProperty("fsExplorerHandle", fsExplorerHandle);
+    engine.rootContext()->setContextProperty("imageStorage", &imageStorage);
+    engine.load(url);
+
+    testFunction();
+
+    return app.exec();
+}
+
+
+Q_DECLARE_METATYPE(KTools::Kff::NameInfo)
+Q_DECLARE_METATYPE(KTools::Kff::NameInfoList)
---
+++ BookCollection/qml/main.qml
@@ -0,0 +1,45 @@
+import QtQuick 2.15
+import QtQuick.Window 2.15
+import "QmlIncludes"
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Image Collection")
+    color: "Black"
+    visibility: "Maximized"
+    KawaiSwitchableWindows {
+        id: switchableWindow
+    }
+
+    Component.onCompleted: {
+        var conf =
+                [
+                    [
+                        "Test",
+                        [
+                            ["FSExplorer", "qrc:/qml/FSExplorer/StartObject.qml", []],
+                            ["Image Storage", "qrc:/qml/ImageStorage/ImageStorage.qml", []]
+                        ]
+                    ]
+                ]
+        switchableWindow.addItemsAndSections(conf)
+    }
+    Connections {
+        target: fsExplorerHandle
+        function onOpenImage(pix)
+        {
+            var conf =
+                    [
+                        [
+                            "Pictures",
+                            [
+                                ["Picture", "qrc:/qml/ImageViewer/ImageViewer.qml", pix]
+                            ]
+                        ]
+                    ]
+            switchableWindow.addItemsAndSections(conf)
+        }
+    }
+}
